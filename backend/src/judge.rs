a8bc89619293e75c8ccf7fd66cda10e6a30a62e4
yanboww <wywkaixin10@gmail.com>
2026-05-29 23:06:15 -0400

    Used docker to run simple program with test input file and wrote result to a result file

--- backend/src/judge.rs
+++ backend/src/judge.rs
@@ -1,9 +1,15 @@
-use tokio::process::Command;
-use std::{fmt::Error, process::{ExitStatus, Stdio}};
+#[allow(unused_imports)]
+use tokio::{process::Command,io::AsyncWriteExt,fs};
+#[allow(unused_imports)]
+use std::{fmt::Error, io::Stdin, path::PathBuf, process::{ExitStatus, Output, Stdio}, thread::spawn};
 use crate::enyay::Problem;
 
 #[tokio::main]
 pub async fn main(){
+    /* 
+        Retrieves current dir based on where cargo run is executed. For this, to work
+        we need to execute in the backend dir
+     */
     let mut input_dir = std::env::current_dir().expect("Failed to retrieve current dir");
     input_dir = input_dir.join("user_input");
     
@@ -15,12 +21,11 @@ pub async fn main(){
     };
 
     let volume_mount = format!("{}:/app",input_dir.display());
-    //let compile = run_code("Hello.cpp").await.unwrap();
-    let _compile = run_with_docker(&test_question, "Hello.cpp", &volume_mount).await;
-    //println!("{:?}",compile);
+    let output = compile_with_docker(&test_question, "AddTwo.cpp", &volume_mount).await;
+    let _write = write_out_to_file(output.trim(),&input_dir).await;
 }   
 
-async fn run_with_docker(question:&Problem, file_name: &str, volume_mount: &str) {
+async fn compile_with_docker(question:&Problem, file_name: &str, volume_mount: &str) -> String {
     let compile = Command::new("docker")
         .args(["run","--rm"])
         .args(["-v",volume_mount])
@@ -32,23 +37,36 @@ async fn run_with_docker(question:&Problem, file_name: &str, volume_mount: &str)
         .expect("Failed to compile code");
     if !compile.success() {panic!("Error compiling");}
     let memory = format!("{}m",question.memory_mb.to_string());
-    let mut child = Command::new("docker")
+    run_with_docker(&memory, volume_mount).await
+}
+
+async fn run_with_docker(memory_limit:&str, volume_mount: &str) -> String{
+    let child = Command::new("docker")
         .args(["run", "-i", "--rm"])       
-        .args(["--memory", &memory])       
+        .args(["--memory", memory_limit])       
         .args(["--cpus", "1.0"])           
         .args(["--network", "none"])       
-        .args(["-v", &volume_mount])
+        .args(["-v", volume_mount])
         .args(["-w", "/app"])
         .arg("gcc:latest")
-        .arg("./a.out")
-        .stdin(Stdio::piped())
+        .args(["sh", "-c", "./a.out < input.txt"])
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn()
         .expect("Failed to run code");
-    drop(child.stdin.take());
+
+
     let output = child.wait_with_output().await.expect("Fail to read output");
-    println!("Container Exit Status: {}", output.status);
-    println!("STDOUT: {}", String::from_utf8_lossy(&output.stdout));
-    println!("STDERR: {}", String::from_utf8_lossy(&output.stderr));
+    let out_str = String::from_utf8_lossy(&output.stdout);
+    out_str.into_owned()
+    //println!("STDOUT: {}", String::from_utf8_lossy(&output.stdout));
 }
+
+async fn write_out_to_file(output: &str, path: &PathBuf){
+    let output = output.trim();
+    let write_path = path.join("user_output.txt");
+    fs::write(write_path,output)
+        .await
+        .expect("Failed to write to disk")
+}
+
---
+++ backend/user_input/AddTwo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+	int n; std::cin >> n;
+	while(n-- > 0) {
+		int val; std::cin >> val;
+		std::cout << (val+2) << "\n";
+	}
+	return 0;
+}
